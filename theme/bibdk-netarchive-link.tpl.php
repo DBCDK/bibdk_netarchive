a05b74041adb4733b0d19afa0180bf81a4726ab9
JrgenGNielsen <jgn@dbc.dk>
2014-02-12 15:11:17 +0100

    use FPDI.php instead of netArchive.py

--- theme/bibdk-netarchive-link.tpl.php
+++ theme/bibdk-netarchive-link.tpl.php
@@ -6,8 +6,11 @@
 
   $elements['#options']['attributes']['class'][] = 'text-small';
   $elements['#options']['attributes']['class'][] = 'text-darkgrey';
+  $elements['#options']['attributes']['class'][] = 'bibdk-popup-link';
 
-  $elements['#options']['attributes']['rel'] = 'lightframe[|width:680px; height:900px; scrolling: auto;]';
+  $elements['#options']['attributes']['data-rel'][] = 'backcoverpdf';
+
+  // $elements['#options']['attributes']['rel'] = 'lightframe[|width:680px; height:900px; scrolling: auto;]';
 
   echo l(
     $elements['#text'],
